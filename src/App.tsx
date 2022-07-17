a3ce188aea9f1c13339bb0534100c3fa870f20e1
Salman H <Salman0112358@outlook.com>
2022-07-17 15:53:35 +0100

    fixing image box and map link

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 //import { greet } from "./utils/greet";
 import PageHeader from "./components/PageHeader"
-//import PictureBox from "./components/PictureBox";
+import PictureBox from "./components/PictureBox";
 
 function App(): JSX.Element {
   return (
-    <PageHeader
-    title ={"Favourite places to vist by Sal"}
+    <>
+        <PageHeader
+        title ={"Favourite places to vist by Sal"}
     />
+
+        <PictureBox
+        imageLink= {"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/d5/1c/94/passu-valley-hunza-valley.jpg?w=1200&h=-1&s=1"}
+        imageTitle= {"Hunza Valley"}
+        country = {"Pakistan"}
+        locale = {"Hunza Valley"}
+        mapLink = {"https://www.google.com/maps?q=hunza+valley&um=1&ie=UTF-8&sa=X&ved=2ahUKEwiwzbWSlYD5AhV8SUEAHbXbCOgQ_AUoAnoECAIQBA"}
+        imageCaption = {"A view of some snow covered mountains and a lake in Hunza Valley Pakistan "}
+        />
+    </>
+
    
   );
 }
--- src/components/PictureBox.tsx
+++ src/components/PictureBox.tsx
@@ -18,7 +18,7 @@ function PictureBox(props : PictureBoxProps ):JSX.Element{
         <div>
             <img src={props.imageLink}></img>
             <h2>{props.imageTitle}</h2>
-            <h3>{props.country} , {props.locale} ({props.mapLink})</h3>
+            <h3>{props.country} , {props.locale} <a href={props.mapLink} target= {'_blank'}  >(Map Link)</a> </h3>
             <p>{props.imageCaption}</p>
         </div>
 
