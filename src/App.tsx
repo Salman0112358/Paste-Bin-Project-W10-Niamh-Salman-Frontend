bf7bae9cc7cdffb9fa06da11ec51d9fdd0536fb3
Salman H <Salman0112358@outlook.com>
2022-07-17 15:12:50 +0100

    exporting reacting component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { greet } from "./utils/greet";
+import PageHeader from "./components/PageHeader"
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+  <PageHeader/>
+  );
 }
 
 export default App;
--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -6,3 +6,5 @@ function PageHeader() : JSX.Element {
 
     )
 }
+
+export default PageHeader
